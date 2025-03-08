4dae79a49364e90b315c93f714ac56ee3d717681
MorenoBen343 <MorenoBen343@gmail.com>
2025-03-07 17:23:48 -0700

    Initial file making + Adding Function
    Added initial file to repository from tutorial as well as the changes in the script file for adding a spawnHealthPot function.

---
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using CreatorKitCode;
+
+public class SpawnerSample : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+
+    void Start()
+    {
+        spawnHealthPot(0);
+        spawnHealthPot(45);
+        spawnHealthPot(90);
+        spawnHealthPot(135);
+    }
+
+    void spawnHealthPot(int angle) {
+        int radius = 5;
+        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        Vector3 spawnPosition = transform.position + direction * radius;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# IDEA310O-Project-2
+ My 2nd Unity Project for IDEA310O
